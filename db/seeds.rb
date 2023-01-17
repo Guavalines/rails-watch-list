f0d277a44e4cc72e7dc3a0d41afab1238c6588eb
guavalines <konstrictor@gmail.com>
2023-01-17 15:15:49 -0800

    added faker seeds and modified controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require "json"
-require "open-uri"
+# require "json"
+# require "open-uri"
 
-base_url = "https://tmdb.lewagon.com/movie/top_rated"
-banner_url = "https://image.tmdb.org/t/p/original"
-10.times do |i|
-movies = JSON.parse(URI.open("#{base_url}?page=#{i + 1}").read)["results"]
-movies.each do |movie|
-  Movie.create(
-    title: movie["title"],
-    overview: movie["overview"],
-    poster_url: "#{banner_url}#{movie['backdrop_path']}",
-    rating: movie["vote-average"]
-    )
-  end
+# base_url = "https://tmdb.lewagon.com/movie/top_rated"
+# banner_url = "https://image.tmdb.org/t/p/original"
+# 10.times do |i|
+# movies = JSON.parse(URI.open("#{base_url}?page=#{i + 1}").read)["results"]
+# movies.each do |movie|
+#   Movie.create(
+#     title: movie["title"],
+#     overview: movie["overview"],
+#     poster_url: "#{banner_url}#{movie['backdrop_path']}",
+#     rating: movie["vote-average"]
+#     )
+#   end
+# end
+
+# ------------------------FAKER------------------
+Movie.delete_all
+
+NUM_MOVIES = 10
+
+NUM_MOVIES.times do
+  title = Faker::Movie.title
+  overview = Faker::Movie.quote
+  Movie.create(title: title, overview: overview)
 end
+
+# --------------------FAKER----------------
+
+# require 'net/http'
+# require 'json'
+
+# 20.times do |i|
+#   url = "http://tmdb.lewagon.com/movie/#{i + 1}"
+#   uri = URI(url)
+#   response = Net::HTTP.get(uri)
+#   repos = JSON.parse(response)
+#   # IMAGE FETCH PROCESS
+#   poster_url = "https://image.tmdb.org/t/p/w300#{repos['poster_path']}"
+#   Movie.create(title: repos["title"], overview: repos["overview"], rating: repos["popularity"], poster_url: poster_url)
+# end
