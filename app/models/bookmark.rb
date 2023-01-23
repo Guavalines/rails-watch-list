ce489c683070191968e9db49c195616c92bb1899
guavalines <konstrictor@gmail.com>
2023-01-22 17:20:39 -0800

    added has attached photo to models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+  has_one_attached :photo
   validates :movie, uniqueness: { scope: :list }
   validates :comment, length: { minimum: 6 }
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
   validates :name, uniqueness: true, presence: true, length: { maximum: 30 }
 end
